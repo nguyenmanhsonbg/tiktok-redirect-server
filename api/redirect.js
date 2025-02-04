2865c86215133acdcd8e6bfcfe363840ede27d47
Manh Nguyen <57060858+nguyenmanhsonbg@users.noreply.github.com>
2025-02-05 00:00:16 +0700

    update

--- api/redirect.js
+++ api/redirect.js
@@ -29,7 +29,7 @@ module.exports = async (req, res) => {
     }
 
     const userAgent = req.headers["user-agent"];
-    let respLink;
+    let respLink = product.webLink;
    
 
     if (/facebookexternalhit/i.test(userAgent)) {
