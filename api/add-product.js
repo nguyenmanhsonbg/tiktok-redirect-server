d7768293e38601632bdb69baca61d6e1def44949
ManhNguyen <57060858+nguyenmanhsonbg@users.noreply.github.com>
2025-11-24 11:47:48 +0700

    Fix

--- api/add-product.js
+++ api/add-product.js
@@ -16,11 +16,7 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
 
-  
   const { webLink1, webLink2 } = req.body;
 
   // Validate input fields
