63b35125bcef476eba7477e9be1663fb83b88e12
ManhNguyen <57060858+nguyenmanhsonbg@users.noreply.github.com>
2026-06-22 15:45:41 +0700

    feat

--- api/get-products.js
+++ api/get-products.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
   }
 
   if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed." });
+    return res.status(405).json({ error: "Method not allowed1." });
   }
 
   try {
